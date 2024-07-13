9b27eaa3b2df484cd28f19bcd7fe6f06a2c9edb5
Suhyun Park <suhyun_4951@naver.com>
2024-07-14 02:11:04 +0900

    create project

---
+++ main.js
@@ -0,0 +1,9 @@
+let str = `
+010-1234-1234
+thesecon@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`;
+
+console.log(str.match(/h..p/g));
